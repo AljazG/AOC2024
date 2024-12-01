0dcabc76295c7ba81a5c9dd71e5515e96491a415
aljazg <aljazgrubar@gmail.com>
2024-12-01 22:29:05 +0100

    aoc day 1

---
+++ main.dart
@@ -0,0 +1,28 @@
+import 'model/advent_problem.dart';
+import 'solutions/day01.dart';
+
+bool showLatest = true;
+
+final days = <AdventProblem>[Day01()];
+
+void checkArgs(List<String?> args) {
+  for (var arg in args) {
+    if (arg.isAllArg()) {
+      showLatest = false;
+    }
+  }
+}
+
+void main(List<String?> args) {
+  checkArgs(args);
+
+  showLatest
+      ? days.last.printSolution()
+      : days.forEach((day) => day.printSolution());
+}
+
+extension argsMatcher on String? {
+  bool isAllArg() {
+    return this == '-a' || this == '-all';
+  }
+}
---
+++ utils/input_util.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+class Input {
+  late String _inputString;
+  late List<String> _inputList;
+
+  Input(int day)
+      : _inputList = _readInputDayAsList(day),
+        _inputString = _readInputDay(day);
+
+  static String _createInputPath(int day) {
+    final dayString = day.toString().padLeft(2, '0');
+    return './input/day$dayString.txt';
+  }
+
+  static String _readInputDay(int day) {
+    return _readInput(_createInputPath(day));
+  }
+
+  static String _readInput(String input) {
+    return File(input).readAsStringSync();
+  }
+
+  static List<String> _readInputDayAsList(int day) {
+    return _readInputAsList(_createInputPath(day));
+  }
+
+  static List<String> _readInputAsList(String input) {
+    return File(input).readAsLinesSync();
+  }
+
+  List<String> getList() => _inputList;
+
+  List<String> getPerWhitespace() {
+    return _inputString.split(RegExp(r'\s\n'));
+  }
+
+  List<String> getBy(String pattern) {
+    return _inputString.split(pattern);
+  }
+
+  List<String> getByRegex(RegExp regex) {
+    return _inputString.split(regex);
+  }
+}

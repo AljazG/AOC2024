214cd177cf699d863722ed39c46450fa6fdf2f5c
aljazgrubar <aljaz.grubar@src.si>
2024-12-04 08:47:38 +0100

    day 3

--- solutions/day03.dart
+++ solutions/day03.dart
@@ -44,6 +44,45 @@ class Day03 extends AdventProblem {
     return (index, 0);
   }
 
+  int getSumP2(String inputStr, int index) {
+    if ((index + 4) >= inputStr.length) {
+      return 0;
+    }
+
+    if (inputStr.substring(index, (index + 4)) == 'mul(') {
+      index += 4;
+      int a;
+      int b;
+      (a, index) = getNum(inputStr, index);
+
+      if (a == -1) {
+        return 0;
+      }
+
+      if (inputStr[index] != ',') {
+        return 0;
+      }
+
+      index++;
+
+      (b, index) = getNum(inputStr, index);
+
+      if (b == -1) {
+        return 0;
+      }
+
+      if (inputStr[index] != ')') {
+        return 0;
+      }
+
+      return a * b;
+    }
+
+    index++;
+
+    return 0;
+  }
+
   (int, int) getNum(String inputStr, int index) {
     String num = '';
 
@@ -63,6 +102,30 @@ class Day03 extends AdventProblem {
     return (int.parse(num), index);
   }
 
+  bool getDont(String inputStr, int index) {
+    if ((index + 7) >= inputStr.length) {
+      return false;
+    }
+
+    if (inputStr.substring(index, (index + 7)) == "don't()") {
+      return true;
+    }
+
+    return false;
+  }
+
+  bool getDo(String inputStr, int index) {
+    if ((index + 4) >= inputStr.length) {
+      return false;
+    }
+
+    if (inputStr.substring(index, (index + 4)) == "do()") {
+      return true;
+    }
+
+    return false;
+  }
+
   int traverseInput(String inputStr) {
     int i = 0;
     int sum = 0;
@@ -75,6 +138,29 @@ class Day03 extends AdventProblem {
     return sum;
   }
 
+  int traverseInputP2(String inputStr) {
+    int i = 0;
+    int sum = 0;
+    int mul;
+    bool dont = false;
+
+    while (i < inputStr.length) {
+      if (!dont) {
+        mul = getSumP2(inputStr, i);
+        sum += mul;
+      } else {
+        dont = !getDo(inputStr, i);
+      }
+
+      if (!dont) {
+        dont = getDont(inputStr, i);
+      }
+
+      i++;
+    }
+    return sum;
+  }
+
   @override
   int solvePart1() {
     String inputStr = input.getString();
@@ -83,6 +169,7 @@ class Day03 extends AdventProblem {
 
   @override
   int solvePart2() {
-    return 0;
+    String inputStr = input.getString();
+    return traverseInputP2(inputStr);
   }
 }
